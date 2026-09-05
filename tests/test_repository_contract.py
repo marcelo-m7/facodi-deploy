53b5943b13706faac5b7293c1c348a792f3bbebc
Marcelo Santos <117441129+marcelo-m7@users.noreply.github.com>
2026-09-05 19:52:03 +0100

    test: expect Monynha M3 deploy pin

--- tests/test_repository_contract.py
+++ tests/test_repository_contract.py
@@ -9,7 +9,7 @@ EXPECTED_SUBMODULES = {
     "addons/facodi-ai": "a9e79332d9876d44c032c65e0525cf4df43306cc",
     "addons/facodi-learning": "c0d66e3d5ee412dddf89e4a9ad64ec2ab6fd9e18",
     "addons/facodi-theme": "be35673a5649f5e6f7b01777905d0899e3daaf7b",
-    "addons/monynha-odoo": "96c03e92a54ca9ca4e4f32a1307fd9bba36949ce",
+    "addons/monynha-odoo": "e2f26bf6e9d4d232b5cb56f4ff1e2a05626bf425",
     "vendor/odoo-design-themes": "a1818df4ade65406c0cacae8b1ea676e6f70095f",
 }
 
