5addaea267d54c853f9bf371f02de302b7126458
Marcelo Santos <117441129+marcelo-m7@users.noreply.github.com>
2026-09-05 01:48:37 +0100

    infra: use configurable staging minimum instances

--- infrastructure/terraform/environments/staging/main.tf
+++ infrastructure/terraform/environments/staging/main.tf
@@ -12,7 +12,7 @@ module "runtime" {
   runtime_enabled              = var.runtime_enabled
   public_access_enabled        = var.public_access_enabled
   initial_image_uri            = var.initial_image_uri
-  min_instances                = 0
+  min_instances                = var.min_instances
   max_instances                = 1
   database_tier                = var.database_tier
   deploy_service_account_email = "facodi-github-deploy@${var.project_id}.iam.gserviceaccount.com"
