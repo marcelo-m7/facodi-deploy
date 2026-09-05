7150188a400e1f23c01e9d4200b4a17081dc03df
Marcelo Santos <117441129+marcelo-m7@users.noreply.github.com>
2026-09-05 19:53:12 +0100

    chore: validate Monynha M3 source pin

--- scripts/validate-repository.sh
+++ scripts/validate-repository.sh
@@ -15,7 +15,7 @@ done
 
 test "$(git -C addons/facodi-learning rev-parse HEAD)" = "c0d66e3d5ee412dddf89e4a9ad64ec2ab6fd9e18"
 test "$(git -C addons/facodi-theme rev-parse HEAD)" = "be35673a5649f5e6f7b01777905d0899e3daaf7b"
-test "$(git -C addons/monynha-odoo rev-parse HEAD)" = "96c03e92a54ca9ca4e4f32a1307fd9bba36949ce"
+test "$(git -C addons/monynha-odoo rev-parse HEAD)" = "e2f26bf6e9d4d232b5cb56f4ff1e2a05626bf425"
 test "$(git -C vendor/odoo-design-themes rev-parse HEAD)" = "a1818df4ade65406c0cacae8b1ea676e6f70095f"
 
 python3 -m unittest tests/test_repository_contract.py tests/test_migration_contract.py -v
